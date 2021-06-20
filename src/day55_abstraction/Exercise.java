0f4f7704bd200f2e36a575bafd22be97b50488e0
Inthira K <kefind@gmail.com>
2021-06-20 11:53:02 -0400

    day55_abstraction 1st committed

---
+++ src/day55_abstraction/Exercise.java
@@ -0,0 +1,15 @@
+package day55_abstraction;
+
+public abstract class Exercise {
+
+    public void start() { //1st call non-abstract method
+        System.out.println("Warming up and starting the exercise");
+    }
+
+    //abstract dont need {} method with out body, just signature
+    //body means implementation, purpose to let sub classes implement/override their own way
+    public abstract void perform();
+
+    public abstract int getCaloriesCount(int minutes); //param - minute for how long exercise perform
+
+}
