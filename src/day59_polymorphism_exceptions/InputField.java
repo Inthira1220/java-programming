5a71e65ba410ec037962f2cd2030c3a6ca422edf
Inthira K <kefind@gmail.com>
2021-07-03 11:56:43 -0400

    day59_polymorphism_exceptions: 1st committed

---
+++ src/day59_polymorphism_exceptions/InputField.java
@@ -0,0 +1,21 @@
+package day59_polymorphism_exceptions;
+
+public class InputField implements WebElement{
+    @Override
+    public void sendKeys(String txt) {
+
+    }
+
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void getText() {
+
+    }
+    public void getValue(){
+        System.out.println();
+    }
+}
---
+++ src/day59_polymorphism_exceptions/Link.java
@@ -0,0 +1,21 @@
+package day59_polymorphism_exceptions;
+
+public class Link implements WebElement{
+    @Override
+    public void sendKeys(String txt) {
+
+    }
+
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void getText() {
+
+    }
+    public void getLinkHref(){
+        System.out.println("");
+    }
+}
