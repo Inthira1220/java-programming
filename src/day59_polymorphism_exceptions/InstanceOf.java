854a508cd61db708d39cf6511d072fb9c5b5bf3b
Inthira K <kefind@gmail.com>
2021-07-03 16:38:50 -0400

    day59_polymorphism_exceptions: added class RuntimeErrorExample

--- src/day59_polymorphism_exceptions/InstanceOf.java
+++ src/day59_polymorphism_exceptions/InstanceOf.java
@@ -8,6 +8,27 @@ public class InstanceOf {
         //instance of operator
         Shape shape = new Circle();
         System.out.println(shape instanceof Circle);
+        //System.out.println(shape instanceof Square);
+
+        if (shape instanceof Circle) {
+            System.out.println("It is a Circle object");
+        //} else if (shape instanceof Square) {
+            System.out.println("It is a Square object");
+        }
+
+        /**
+         * java reflection API : https://docs.oracle.com/javase/tutorial/reflect/index.html
+         */
+        System.out.println(shape.getClass().getSimpleName()); //just class name of object
+        System.out.println(shape.getClass().getName());// class name of object and package name
+        if (shape.getClass().getSimpleName().equals("Square")) {
+            System.out.println("It is a square object");
+        }
+        System.out.println(shape.getClass().getDeclaredMethods().length);
+
+        WebElement el = new Link();
+        System.out.println("object class name of el variable = " + el.getClass().getSimpleName());
+        //System.out.println(el.getClass().getDeclaredFields());
 
         }
 }
---
+++ src/day59_polymorphism_exceptions/exeption_handling/RuntimeErrorExample.java
@@ -0,0 +1,4 @@
+package day59_polymorphism_exceptions.exeption_handling;
+
+public class RuntimeErrorExample {
+}
