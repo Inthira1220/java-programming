1840b25048be1ba63291366fdad3f8d98c0bc99b
Inthira K <kefind@gmail.com>
2021-07-03 17:24:23 -0400

    final committed

--- src/day59_polymorphism_exceptions/exeption_handling/RuntimeErrorExample.java
+++ src/day59_polymorphism_exceptions/exeption_handling/RuntimeErrorExample.java
@@ -1,4 +1,9 @@
 package day59_polymorphism_exceptions.exeption_handling;
 
 public class RuntimeErrorExample {
+    public static void main(String[] args) {
+        System.out.println(10 / 2);
+        System.out.println(10 / 0);  //ArithmeticException, RuntimeErrorExample.java:6
+        System.out.println(10 / 3);
+    }
 }
---
+++ src/day59_polymorphism_exceptions/exeption_handling/TryCatch.java
@@ -0,0 +1,29 @@
+package day59_polymorphism_exceptions.exeption_handling;
+
+public class TryCatch {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(10 / 2);
+            System.out.println(10 / 0);
+            System.out.println(10 / 3);
+        } catch (Exception e) {
+            System.out.println("RuntimeException happened and handled");
+        }
+        System.out.println("After first try catch");
+
+
+        //array example
+
+        int[] nums = {11, 33, 44};
+        try {
+            System.out.println(nums[0]);
+            System.out.println(nums[2]);
+            System.out.println(nums[3]);
+            System.out.println(nums[1]); // this line cant run, its also skipped
+        }catch (ArrayIndexOutOfBoundsException e){ //e is the name we can change to anything you like
+            System.out.println("ArrayIndexOutOfBoundsException is caught and handled");
+        }
+        System.out.println("After array code");
+    }
+}
